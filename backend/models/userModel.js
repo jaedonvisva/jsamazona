35a537edd6b3f519553818c8b3ed07a174787cfe
Basir <basir.jafarzadeh@gmail.com>
2020-06-22 21:41:04 +0430

    add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+const userModel = mongoose.model(userSchema);
+
+export default userModel;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import products from './data';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
+
+mongoose.connect(
+  'mongodb://localhost/jsamazona',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (e) => {
+    if (e) {
+      console.log('MongoDB Error', e);
+    } else {
+      console.log('DB Connected');
+    }
+  }
+);
+
 app.get('/api/products', (req, res) => {
   res.send(products);
 });
