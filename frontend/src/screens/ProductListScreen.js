d371a242f3a3bd6aae89b7b8c77705ac05a6d700
Basir <basir.jafarzadeh@gmail.com>
2020-07-26 21:31:41 +0430

    create product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,4 +1,4 @@
-import { createProduct } from '../api';
+import { createProduct, getProducts } from '../api';
 
 const ProductListScreen = {
   after_render: () => {
@@ -9,11 +9,42 @@ const ProductListScreen = {
         alert(data.product._id);
       });
   },
-  render: () => {
+  render: async () => {
+    const products = await getProducts();
     return `
     <div>
       <h1>Products</h1>
       <button id="create-product-button">Create Product</button>
+      <div class="product-list">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th class="tr-action">ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${products.map(
+              (product) => `
+            <tr>
+              <td>${product._id}</td>
+              <td>${product.name}</td>
+              <td>${product.price}</td>
+              <td>${product.category}</td>
+              <td>${product.brand}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+            `
+            )}
+          </tbody>
+      </div>
     </div>`;
   },
 };
